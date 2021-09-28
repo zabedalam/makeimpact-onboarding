1fa525a0ef77c80d7063b43ac46dd00cfee54d23
zabedalam <zabedalam777@gmail.com>
2021-09-28 23:51:50 +0200

    Nested object adding logic done

--- src/components/pages/UserDetails.tsx
+++ src/components/pages/UserDetails.tsx
@@ -9,21 +9,49 @@ const UserDetails = () => {
   const [iso_code, setIso_code] = useState("");
   const [countryName, setCountryName] = useState("");
   console.log("user", user);
+  //   console.log('iso',iso_code,setIso_code)
 
   const submit = async (e: any) => {
     e.preventDefault();
-    const userRef = firestore.doc(`users-tp/${user}`);
+    // const userRef = firestore.doc(`users-tp/${user?.id}`);
+  //   const currentUserId = user?.id;
+    const userRef = firestore.collection('users-tp').doc(user?.id)
+
     try {
       await userRef.set({
         country: {
           iso_code: iso_code,
           countryName: countryName,
         },
-      });
+      },{merge:true});
     } catch (error) {
       console.log("Error in creating user", error);
     }
   };
+
+  // const submit = () => {
+  //   // const currentUserId = store.getState().auth.uid;
+  //   const currentUserId = user?.id;
+  //   // console.log('currentuser',currentUserId)
+
+  //     firestore
+  //     .collection("users-tp")
+  //     .get()
+  //     .then((usersSnapshot) => {
+  //        usersSnapshot.forEach((doc) => {
+  //         // looking for the current user and then updating their data
+  //         if (doc.data().uid === currentUserId) {
+  //           firestore
+  //             .collection("users-tp")
+  //             .doc(doc.id)
+  //             .set({
+  //               country: {iso_code, countryName }
+  //             },{merge:true});
+  //         }
+  //       });
+  //     });
+  // };
+
   return (
     <div>
       <div className="App__form">
--- src/components/pages/UserInfo.tsx
+++ src/components/pages/UserInfo.tsx
@@ -4,26 +4,50 @@ import { firestore } from "../../firebase/config";
 
 import { RootState } from "../../store";
 
-const UserDetails = () => {
+const UserInfo = () => {
   const { user } = useSelector((state: RootState) => state.auth);
   const [city, setCity] = useState("");
   const [street, setStreet] = useState("");
   console.log("user", user);
+  console.log("city", city, setCity);
+    const submit = async (e: any) => {
+      e.preventDefault();
+      // const userRef = firestore.doc(`users-tp/${user}`);
+      const userRef = firestore.collection('users-tp').doc(user?.id)
 
-  const submit = async (e: any) => {
-    e.preventDefault();
-    const userRef = firestore.doc(`users-tp/${user}`);
-    try {
-      await userRef.set({
-        separate_mailling_address: {
-          city: city,
-          street: street,
-        },
-      });
-    } catch (error) {
-      console.log("Error in creating user", error);
-    }
-  };
+      try {
+        await userRef.set({
+          separate_mailling_address: {
+            city: city,
+            street: street,
+          },
+        },{merge:true});
+      } catch (error) {
+        console.log("Error in creating user", error);
+      }
+    };
+
+  // const submit = () => {
+  //   // const currentUserId = store.getState().auth.uid;
+  //   const currentUserId = user?.id;
+
+  //    firestore
+  //     .collection("users-tp")
+  //     .get()
+  //     .then((usersSnapshot) => {
+  //       usersSnapshot.forEach((doc) => {
+  //         // looking for the current user and then updating their data
+  //         if (doc.data().uid === currentUserId) {
+  //           firestore.collection("users-tp").doc(doc.id).set(
+  //             {
+  //               separate_mailing_address: { city, street },
+  //             },
+  //             { merge: true }
+  //           );
+  //         }
+  //       });
+  //     });
+  // };
   return (
     <div>
       <div className="App__form">
@@ -45,4 +69,4 @@ const UserDetails = () => {
   );
 };
 
-export default UserDetails;
+export default UserInfo;
